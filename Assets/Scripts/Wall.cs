7b95816c4d1df23edec8f3f89d9f992d523a7172
Boris Shishov <borisshishov@gmail.com>
2018-08-13 22:02:09 +0200

    Fixed walls

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -9,30 +9,38 @@ namespace Assets.Scripts
         private Transform _cameraTransform;
         private MeshRenderer _meshRenderer;
 
-        private bool _isVisible;
+        private bool _isVisible = true;
 
-        void Start ()
+        private void Start()
         {
             _cameraTransform = Camera.main.transform;
             _meshRenderer = GetComponent<MeshRenderer>();
 
-            StartCoroutine(LateStart());
+            //StartCoroutine(LateStart());
+            Check();
         }
 
         IEnumerator LateStart()
         {
+            //yield return new WaitForSeconds(2f);
             yield return new WaitForEndOfFrame();
+            //yield return null;
+            Debug.Log("CHECK!");
             Check();
         }
         
-        void Update ()
+        void Update()
         {
             Check();
         }
 
         void Check()
         {
-            var isFacingCamera = Vector3.Dot(transform.forward, _cameraTransform.position - _cameraTransform.forward) > 0f;
+            Debug.DrawRay(_cameraTransform.position, _cameraTransform.forward, Color.green);
+            Debug.DrawRay(transform.position, transform.forward, Color.yellow);
+            var xyT = new Vector2(transform.forward.x, transform.forward.z);
+            var xyC = new Vector2(_cameraTransform.forward.x, _cameraTransform.forward.z);
+            var isFacingCamera = Vector2.Dot(xyT, xyC) < 0;
 
             if (isFacingCamera && !_isVisible)
             {
